4c0f13909faf43a926ca9445168a38652de55e21
RIYA PRASAD <RIYAPRASAD@mac.home>
2026-05-08 22:17:51 +0100

    Add pattern printing programs Pt4 to Pt7

---
+++ Loop_&_Functions/Patterns/Pt5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+
+    for(int i=0;i<=n;i++){  
+        
+        for (int j=1;j<=n; j++){
+            if(j==1 || j==n||i==0||i==n){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}

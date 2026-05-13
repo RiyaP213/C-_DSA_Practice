f39ca9559bbc81e7dd87f55e44d26d2082cf4957
RIYA PRASAD <RIYAPRASAD@MacBook-Pro.local>
2026-05-13 21:58:21 +0100

    Functions folder and template added

---
+++ Loop_&_Functions/Functions/FunctionSyntax.cpp
@@ -0,0 +1,13 @@
+// A function is a block of code that performs a specific task. It is a reusable piece of code that can 
+//be called multiple times in a program. Functions help to break down a complex problem into smaller, 
+//manageable parts, making the code more organized and easier to read.    
+
+#include <iostream>
+using namespace std;
+void SayHello(){
+    cout << "Hello, World!" << endl;
+    }
+    int main(){
+        SayHello(); // calling the function
+        return 0;
+    }
